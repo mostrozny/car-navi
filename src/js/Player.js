20b254b5179fa749c601b99279a0728356a3bd36
mostrozny <ostrozny.michal@gmail.com>
2018-08-29 15:17:51 +0200

    player interface

--- src/js/Player.js
+++ src/js/Player.js
@@ -4,19 +4,79 @@ import flirtini from '../images/flirtini.jpg';
 import biggie from '../images/biggie.jpg';
 
 class Player extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            thumbs: [flirtini, thumbGreta, biggie],
+            infoTrack: ["Flirtini - Hearthbreaks & Promises IV", "Greta Van Fleet - From The Fires", "Notorious B.I.G - Ready to die"],
+            playing: true,
+        }
+    }
+
+
+    handlePrevTrack = () => {
+        //miniatures
+        let thumbsArray = this.state.thumbs;
+        const thumbsLength = thumbsArray.length;
+        thumbsArray = [thumbsArray[thumbsLength-1], ...thumbsArray];
+        thumbsArray.splice(thumbsLength);
+
+        //info
+        let infoArray = this.state.infoTrack;
+        const infoLength = infoArray.length;
+        infoArray = [infoArray[infoLength-1], ...infoArray];
+        infoArray.splice(infoLength);
+
+        //setting state
+        this.setState({
+            thumbs: thumbsArray,
+            infoTrack: infoArray,
+        })
+    };
+
+    handleNextTrack = () => {
+        //miniatures
+        let thumbsArray = this.state.thumbs;
+        thumbsArray = [...thumbsArray, thumbsArray[0]];
+        thumbsArray.splice(0, 1);
+
+
+        //info
+        let infoArray = this.state.infoTrack;
+        infoArray = [...infoArray, infoArray[0]];
+        infoArray.splice(0, 1);
+
+        //setting state
+        this.setState({
+            thumbs: thumbsArray,
+            infoTrack: infoArray,
+        })
+    };
+
+
+
     render() {
-        const {thumbnail, infoTrack} = this.props;
-        console.log(thumbnail)
+        const {thumbs, infoTrack} = this.state;
 
         return (
             <div className="player">
-                <div className="prevTrack"><img src={flirtini} className="thumbnail"/></div>
-                <div className="currentTrack">
-                <div className="header">Now playing...</div>
-                <div className="thumbnail"><img src={thumbGreta} className="thumbnail"/></div>
-                <div className="infoTrack">{infoTrack} </div>
+                <div onClick={this.handlePrevTrack} className="prevTrack">
+                    <img src={thumbs[0]} className="thumbnail"/>
                 </div>
-                <div className="nextTrack"><img src={biggie} className="thumbnail"/></div>
+
+                <div className="currentTrack">
+                  <div className="header">Now playing...</div>
+                  <div className="thumbnail"><img src={thumbs[1]} className="thumbnail"/></div>
+                   <div className="infoTrack">{infoTrack[1]} </div>
+                    <div className="controlPanel">
+                        <i className="far fa-play-circle"></i>
+                        <i className="far fa-pause-circle"></i>
+                    </div>
+                </div>
+                <div onClick={this.handleNextTrack} className="nextTrack">
+                    <img src={thumbs[2]} className="thumbnail"/>
+                </div>
             </div>
         );
     }
