66706f964ffe7edf6e15930aa673de1f6aa02e08
mostrozny <ostrozny.michal@gmail.com>
2018-08-29 16:33:46 +0200

    Added working player, changing tracks etc.

--- src/js/Player.js
+++ src/js/Player.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Sound from 'react-sound';
+
 import thumbGreta from '../images/greta.jpg';
 import flirtini from '../images/flirtini.jpg';
 import biggie from '../images/biggie.jpg';
+/*import gvfmp from '../music/gvfht.mp3';
+import brtdmp from '../music/brtd.mp3';
+import fsmp from '../music/fs.mp3';*/
+
+
 
 class Player extends React.Component {
     constructor(props) {
@@ -10,7 +17,9 @@ class Player extends React.Component {
         this.state = {
             thumbs: [flirtini, thumbGreta, biggie],
             infoTrack: ["Flirtini - Hearthbreaks & Promises IV", "Greta Van Fleet - From The Fires", "Notorious B.I.G - Ready to die"],
-            playing: true,
+            playing: "PLAYING",
+            track: ['../src/music/fs.mp3', '../src/music/gvfht.mp3', '../src/music/brtd.mp3'],
+            playerText: "Now playing...",
         }
     }
 
@@ -28,10 +37,17 @@ class Player extends React.Component {
         infoArray = [infoArray[infoLength-1], ...infoArray];
         infoArray.splice(infoLength);
 
+        //tracks
+        let tracks = this.state.track;
+        const tracksLength = tracks.length;
+        tracks = [tracks[tracksLength-1], ...tracks];
+        tracks.splice(tracksLength);
+
         //setting state
         this.setState({
             thumbs: thumbsArray,
             infoTrack: infoArray,
+            track: tracks,
         })
     };
 
@@ -47,14 +63,33 @@ class Player extends React.Component {
         infoArray = [...infoArray, infoArray[0]];
         infoArray.splice(0, 1);
 
+        //tracks
+        let tracks = this.state.track;
+        tracks = [...tracks, tracks[0]];
+        tracks.splice(0, 1);
+
+
         //setting state
         this.setState({
             thumbs: thumbsArray,
             infoTrack: infoArray,
+            track: tracks,
         })
     };
 
+    handlePlay = () => {
+        this.setState({
+            playing: "PLAYING",
+            playerText: "Now playing...",
+        })
+    }
 
+    handlePause = () => {
+        this.setState({
+            playing: "PAUSED",
+            playerText: "PAUSED",
+        })
+    }
 
     render() {
         const {thumbs, infoTrack} = this.state;
@@ -66,17 +101,22 @@ class Player extends React.Component {
                 </div>
 
                 <div className="currentTrack">
-                  <div className="header">Now playing...</div>
+                  <div className="header">{this.state.playerText}</div>
                   <div className="thumbnail"><img src={thumbs[1]} className="thumbnail"/></div>
                    <div className="infoTrack">{infoTrack[1]} </div>
                     <div className="controlPanel">
-                        <i className="far fa-play-circle"></i>
-                        <i className="far fa-pause-circle"></i>
+                        <i onClick={this.handlePlay} className="far fa-play-circle"/>
+                        <i onClick={this.handlePause} className="far fa-pause-circle"/>
+                        <Sound
+                            url={this.state.track[1]}
+                            playStatus={this.state.playing}
+                        />
                     </div>
                 </div>
                 <div onClick={this.handleNextTrack} className="nextTrack">
                     <img src={thumbs[2]} className="thumbnail"/>
                 </div>
+
             </div>
         );
     }
